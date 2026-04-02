b4797fe420efc46b8b801c5d7cc94c6761db2f9c
AndresofAstora <spencerandres@hotmail.com>
2026-04-01 22:03:54 -0600

    Update megabyte-data.js

--- js/megabyte-data.js
+++ js/megabyte-data.js
@@ -1,12 +1,37 @@
 const MEGABYTE_MAPS = {
   map1: {
     title: "Electro Caves 1",
-    image: "../assets/megabyte-map1.png",
+    image: "../assets/megabyte-map1.jpg",
     categories: {
       Start: [{coords:[100,200], label:"Start Point"}],
       End: [{coords:[800,1200], label:"End Point"}],
       Checkpoint: [{coords:[400,600], label:"Checkpoint 1"}],
-      "Bit Chest": [{coords:[500,900], label:"Bit Chest 1"}]
+      BitChest: [{coords:[500,900], label:"Bit Chest 1"}],
+      PartChest: [{coords:[600,700], label:"Part Chest 1"}],
+      ColorCapsule: [{coords:[300,500], label:"Color Capsule 1"}]
     }
-  }
+  },
+  map2: {
+    title: "Electro Caves 2",
+    image: "../assets/megabyte-map2.jpg",
+      categories: {
+      Start: [{coords:[100,200], label:"Start Point"}],
+      End: [{coords:[800,1200], label:"End Point"}],
+      Checkpoint: [{coords:[400,600], label:"Checkpoint 1"}],
+      BitChest: [{coords:[500,900], label:"Bit Chest 1"}],
+      PartChest: [{coords:[600,700], label:"Part Chest 1"}],
+      ColorCapsule: [{coords:[300,500], label:"Color Capsule 1"}]
+    }
+  },
+  map3: {
+    title: "Electro Caves 3",
+    image: "../assets/megabyte-map3.jpg",
+       categories: {
+      Start: [{coords:[100,200], label:"Start Point"}],
+      End: [{coords:[800,1200], label:"End Point"}],
+      Checkpoint: [{coords:[400,600], label:"Checkpoint 1"}],
+      BitChest: [{coords:[500,900], label:"Bit Chest 1"}],
+      PartChest: [{coords:[600,700], label:"Part Chest 1"}],
+      ColorCapsule: [{coords:[300,500], label:"Color Capsule 1"}]
+    }
 };
