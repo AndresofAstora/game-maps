3c5dfe648aa6b8227bd64b7005d0395e21bef9fd
AndresofAstora <spencerandres@hotmail.com>
2026-04-01 21:16:17 -0600

    Update megabyte-data.js

--- js/megabyte-data.js
+++ js/megabyte-data.js
@@ -1,76 +1,12 @@
 const MEGABYTE_MAPS = {
   map1: {
     title: "Electro Caves 1",
-    image: "../assets/megabyte-map1.png",
+    image: "../assets/megabyte-map1.jpg",
     categories: {
-      Start: [
-        { coords: [300, 400], label: "Start" }
-      ],
-      End: [
-        { coords: [500, 600], label: "End" }
-      ],
-      Ckeckpoint: [
-        { coords: [700, 200], label: "Checkpoint" }
-      ],
-       Bit Chest: [
-        { coords: [800, 300], label: "Bit Chest" }
-      ],
-       Part Chest: [
-        { coords: [900, 400], label: "Part Chest" }
-      ],
-       Color Capsule: [
-        { coords: [1000, 500], label: "Color Capsule" }
-      ]
-    }
-  },
-
-  map2: {
-    title: "Electro Caves 2",
-    image: "../assets/megabyte-map2.png",
-    categories: {
-      Start: [
-        { coords: [300, 400], label: "Start" }
-      ],
-      End: [
-        { coords: [500, 600], label: "End" }
-      ],
-      Ckeckpoint: [
-        { coords: [700, 200], label: "Checkpoint" }
-      ],
-       Bit Chest: [
-        { coords: [800, 300], label: "Bit Chest" }
-      ],
-       Part Chest: [
-        { coords: [900, 400], label: "Part Chest" }
-      ],
-       Color Capsule: [
-        { coords: [1000, 500], label: "Color Capsule" }
-      ]
-    }
-  },
-
-  map3: {
-    title: "Electro Caves 3",
-    image: "../assets/megabyte-map3.png",
-    categories: {
-      Start: [
-        { coords: [300, 400], label: "Start" }
-      ],
-      End: [
-        { coords: [500, 600], label: "End" }
-      ],
-      Ckeckpoint: [
-        { coords: [700, 200], label: "Checkpoint" }
-      ],
-       Bit Chest: [
-        { coords: [800, 300], label: "Bit Chest" }
-      ],
-       Part Chest: [
-        { coords: [900, 400], label: "Part Chest" }
-      ],
-       Color Capsule: [
-        { coords: [1000, 500], label: "Color Capsule" }
-      ]
+      Start: [{coords:[100,200], label:"Start Point"}],
+      End: [{coords:[800,1200], label:"End Point"}],
+      Checkpoint: [{coords:[400,600], label:"Checkpoint 1"}],
+      "Bit Chest": [{coords:[500,900], label:"Bit Chest 1"}]
     }
   }
 };
