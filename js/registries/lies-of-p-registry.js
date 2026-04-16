0c9d3fd660bcb17b25fc453892f01f6652bdebda
AndresofAstora <spencerandres@hotmail.com>
2026-04-16 10:33:11 -0600

    Update lies-of-p-registry.js

--- js/registries/lies-of-p-registry.js
+++ js/registries/lies-of-p-registry.js
@@ -222,39 +222,7 @@ const maps = [
         title: "St. Frangelico Cathedral Library",
         image: "stfrangelicocathedrallibrary.webp",
         page: "maps/stfrangelicocathedrallibrary.html",
-        markers: [
-			{ x:000, y:000, type: "area", tooltip: "000" },
-			{ x:000, y:000, type: "area", tooltip: "000" },
-        	{ x:000, y:000, type: "stargazer", tooltip: "000" },
-			{ x:000, y:000, type: "stargazer", tooltip: "000" },
-	    	{ x:000, y:000, type: "npc", tooltip: "000" },
-            { x:000, y:000, type: "consumable", tooltip: "000" },
-            { x:000, y:000, type: "tool", tooltip: "000" },
-            { x:000, y:000, type: "weapon", tooltip: "000" },
-            { x:000, y:000, type: "defencepart", tooltip: "000" },
-            { x:000, y:000, type: "material", tooltip: "000" },
-            { x:000, y:000, type: "collectable", tooltip: "000" },
-            { x:000, y:000, type: "amulet", tooltip: "000" },
-            { x:000, y:000, type: "costume", tooltip: "000" },
-            { x:000, y:000, type: "gesture", tooltip: "000" },
-            { x:000, y:000, type: "dimensionalbutterfly", tooltip: "000" },
-            { x:000, y:000, type: "crackscalling" },
-            { x:000, y:000, type: "boss", tooltip: "000" },
-            { x:000, y:000, type: "ladder" },
-            { x:000, y:000, type: "lift" },
-            { x:000, y:000, type: "lockeddoor", tooltip: "000"},
-	    	{
-                x:1453,
-                y:959,
-                type: "boss",
-                tooltip: {
-                    title: "Fallen Archbishop Andreus",
-                    items: [
-                        "Twisted Angel's Ergo"
-                    ]
-                }
-            },
-		]
+        markers: []
     },
     {
         id: "pathofthepilgrim",
